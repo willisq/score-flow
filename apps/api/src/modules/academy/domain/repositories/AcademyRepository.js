7b9b9862c299bd109749aa5a31f206eec7a3b259
William <williamsuarezdev@gmail.com>
2025-07-03 23:58:57 -0400

    feat(api-academy): create domain initial implementation

---
+++ apps/api/src/modules/academy/domain/repositories/AcademyRepository.js
@@ -0,0 +1,21 @@
+export class AcademyRepository {
+    async create(academy) {
+        throw new Error("Método create no implementado");
+    }
+
+    async findById(id) {
+        throw new Error("Método findById no implementado");
+    }
+
+    async findByName(name) {
+        throw new Error("Método findByName no implementado");
+    }
+
+    async update(academy) {
+        throw new Error("Método update no implementado");
+    }
+
+    async delete(id) {
+        throw new Error("Método delete no implementado");
+    }
+}
