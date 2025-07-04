e913f509eaba1f24ee0ba596927e13df700ad396
William <USRSIS0118@ESTSIS0118>
2025-07-04 16:19:10 -0400

    feat(academy): add use cases for academy

---
+++ apps/api/src/modules/academy/application/CreateAcademy.js
@@ -0,0 +1,20 @@
+import { randomUUID } from "crypto";
+
+import { Academy } from "#academy/domain/entities/Academy.js";
+import { Name } from "#academy/domain/valuesObjects/Name.js";
+
+export class CreateAcademy {
+  constructor({ academyRepository }) {
+    this.academyRepository = academyRepository;
+  }
+
+  async execute({ name, instructorId }) {
+    const academy = new Academy({
+      id: randomUUID(),
+      name: new Name(name),
+      instructor: instructorId,
+    });
+
+    return await this.academyRepository.create(academy);
+  }
+}
---
+++ apps/api/src/modules/academy/application/UpdateAcademy.js
@@ -0,0 +1,30 @@
+export class UpdateAcademy {
+  constructor({ academyRepository, personRepository }) {
+    this.academyRepository = academyRepository;
+    this.personRepository = personRepository;
+  }
+
+  async execute(id, { name, instructorId }) {
+    const academy = await this.academyRepository.findById(id);
+
+    if (!academy) {
+      throw new Error("Academy not found");
+    }
+
+    if (instructorId) {
+      const instructorExists = await this.personRepository.findById(
+        instructorId
+      );
+      if (!instructorExists) {
+        throw new Error("Instructor not found");
+      }
+      academy.changeInstructor(instructorId);
+    }
+
+    if (name) {
+      academy.changeName(name);
+    }
+
+    return await this.academyRepository.update(academy);
+  }
+}
