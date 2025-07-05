aaab23403871135b9767fd3b198e450fbc661164
William <williamsuarezdev@gmail.com>
2025-07-04 21:50:25 -0400

    feat(academy-usecase): add FindAcademy usecase

--- apps/api/src/modules/academy/application/FIndAcademy.js
+++ apps/api/src/modules/academy/application/FIndAcademy.js
@@ -1,9 +1,26 @@
 export class FindAcademy {
-  constructor({ academyRepository }) {
-    this.academyRepository = academyRepository;
-  }
+	constructor({ academyRepository }) {
+		this.academyRepository = academyRepository;
+	}
 
-  async execute(id) {
-    return await this.academyRepository.findById(id);
-  }
+	async execute(params) {
+		const { id, name, instructorId, instructorName } = params;
+		if (id) {
+			return await this.academyRepository.findById(id);
+		}
+
+		if (name) {
+			return await this.academyRepository.findByName(name);
+		}
+
+		if (instructorId) {
+			return await this.academyRepository.findByInstructorId(instructorId);
+		}
+
+		if (instructorName) {
+			return await this.academyRepository.findByInstructorName(instructorName);
+		}
+
+		return await this.academyRepository.findAll();
+	}
 }
