1f6fe6c0393eb9c3c659b87222fcd2060cfd89c6
William <williamsuarezdev@gmail.com>
2025-07-05 14:35:57 -0400

    feat(championship): create migration file

---
+++ packages/backend/migrations/src/migrations/20250705021426_create_championship.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async (knex) => {
+	await knex.schema.createTable("championship", (table) => {
+		table.uuid("id").primary();
+		table.string("description").notNullable();
+		table.timestamps(true, true);
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async (knex) => {
+	await knex.schema.dropTableIfExists("championship");
+};
