56253fe85ebbaf94dbe90c36d193bbe350addca4
William <williamsuarezdev@gmail.com>
2025-07-04 21:47:41 -0400

    feat(academy): add route to main router

--- apps/api/src/routes/index.js
+++ apps/api/src/routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { PersonRouter } from "#person/interfaces/routes/person.routes.js";
+import { AcademyRouter } from "#academy/interfaces/routes/academy.routes.js";
 
 export const router = Router();
 
 router.use("/person", PersonRouter);
+router.use("/academy", AcademyRouter);
