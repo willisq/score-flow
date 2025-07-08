442a2cb02a53093772b5c02dc7b706f54d5b7f0b
William <williamsuarezdev@gmail.com>
2025-07-07 23:51:32 -0400

    feat(category): add use case to query all categories

--- apps/api/src/modules/category/interfaces/controllers/CategoryController.js
+++ apps/api/src/modules/category/interfaces/controllers/CategoryController.js
@@ -2,6 +2,7 @@ import { databaseService } from "#database/databaseService.js";
 import { UnitOfWork } from "#database/UnitOfWork.js";
 
 import { CreateBulkCategories } from "#category/application/CreateCategory.js";
+import { findAllCategories } from "#category/application/FindAllCategories.js";
 
 import { CategoryDatabaseRepository } from "#category/infraestructure/CategoryDatabaseRepository.js";
 import { CategorySexDatabaseRepository } from "#category/infraestructure/CategorySexDatabaseRepository.js";
@@ -20,9 +21,20 @@ export class CategoryController {
 		});
 
 		const useCase = new CreateBulkCategories({ unitOfWork });
-		
+
 		const createdCategories = await useCase.execute(req.body);
 
 		return res.status(201).json(createdCategories);
+	}
+
+	static async findAll(req, res) {
+		const categoryRepository = new CategoryDatabaseRepository({
+			databaseService,
+		});
+		const useCase = new findAllCategories({ categoryRepository });
+
+		const categories = await useCase.execute();
+
+		return res.status(200).json(categories);
 	}
 }
