94f5ed86cc85d30562fb538f58b661f688b6638a
William <USRSIS0118@ESTSIS0118>
2025-07-10 15:37:15 -0400

    feat(category): add create category use case

--- apps/api/src/modules/category/application/CreateCategory.js
+++ apps/api/src/modules/category/application/CreateCategory.js
@@ -10,71 +10,75 @@ import { CategoryHeight } from "#category/domain/valueObjects/CategoryHeight.js"
 import { CategorySpecialCondition } from "#category/domain/valueObjects/CategorySpecialCondition.js";
 
 export class CreateBulkCategories {
-	constructor({ unitOfWork }) {
-		this.unitOfWork = unitOfWork;
-	}
+  constructor({ unitOfWork }) {
+    this.unitOfWork = unitOfWork;
+  }
 
-	async execute(categoriesData) {
-		if (!Array.isArray(categoriesData) || categoriesData.length === 0) {
-			throw new Error("Input must be a non-empty array of categories.");
-		}
+  async execute(categoriesData) {
+    if (!Array.isArray(categoriesData) || categoriesData.length === 0) {
+      throw new Error("Input must be a non-empty array of categories.");
+    }
 
-		return this.unitOfWork.execute(async (repos) => {
-			const createdCategories = await Promise.all(
-				categoriesData.map(async (data) => {
-					const { categoryData, sexes, ranks } = data;
+    return this.unitOfWork.execute(async (repos) => {
+      const createdCategories = await Promise.all(
+        categoriesData.map(async (data) => {
+          const { categoryData, sexes, ranks } = data;
 
-					if (!sexes || sexes.length === 0) {
-						throw new Error(
-							"La categoria debe poseer al menos un sexo asociado.",
-						);
-					}
-					if (!ranks || ranks.length === 0) {
-						throw new Error(
-							`La categoria debe poseer al menos un rango asociado.`,
-						);
-					}
+          if (!sexes || sexes.length === 0) {
+            throw new Error(
+              "La categoria debe poseer al menos un sexo asociado."
+            );
+          }
+          if (!ranks || ranks.length === 0) {
+            throw new Error(
+              `La categoria debe poseer al menos un rango asociado.`
+            );
+          }
 
-					const categoryInstance = new Category({
-						id: randomUUID(),
-						initialAge: new CategoryAge(categoryData.initialAge),
-						finalAge: new CategoryAge(categoryData.finalAge),
-						initialWeight: new CategoryWeight(categoryData.initialWeight),
-						finalWeight: new CategoryWeight(categoryData.finalWeight),
-						initialHeight: new CategoryHeight(categoryData.initialHeight),
-						finalHeight: new CategoryHeight(categoryData.finalHeight),
-						specialCondition: new CategorySpecialCondition(
-							categoryData.specialCondition,
-						),
-						modality: categoryData.modality,
-					});
+          for (const modality of data.modalities) {
+            const categoryInstance = new Category({
+              id: randomUUID(),
+              initialAge: new CategoryAge(categoryData.initialAge),
+              finalAge: new CategoryAge(categoryData.finalAge),
+              initialWeight: new CategoryWeight(categoryData.initialWeight),
+              finalWeight: new CategoryWeight(categoryData.finalWeight),
+              initialHeight: new CategoryHeight(categoryData.initialHeight),
+              finalHeight: new CategoryHeight(categoryData.finalHeight),
+              specialCondition: new CategorySpecialCondition(
+                categoryData.specialCondition
+              ),
+              modality: modality,
+            });
 
-					const createdCategory =
-						await repos.categoryRepository.create(categoryInstance);
+            const createdCategory = await repos.categoryRepository.create(
+              categoryInstance
+            );
 
-					const categoryWithSexes = sexes.map(
-						(sex) => new CategorySex({ sex, category: categoryInstance.id }),
-					);
+            const categoryWithSexes = sexes.map(
+              (sex) => new CategorySex({ sex, category: categoryInstance.id })
+            );
 
-					const createdCategoriesWithSexes =
-						await repos.categorySexRepository.create(categoryWithSexes);
+            const createdCategoriesWithSexes =
+              await repos.categorySexRepository.create(categoryWithSexes);
 
-					const categoryWithRanks = ranks.map(
-						(rank) => new CategoryRank({ category: categoryInstance.id, rank }),
-					);
+            const categoryWithRanks = ranks.map(
+              (rank) =>
+                new CategoryRank({ category: categoryInstance.id, rank })
+            );
 
-					const createdCategoriesWithRanks =
-						await repos.categoryRankRepository.create(categoryWithRanks);
+            const createdCategoriesWithRanks =
+              await repos.categoryRankRepository.create(categoryWithRanks);
 
-					return {
-						...createdCategory,
-						sexes: createdCategoriesWithSexes,
-						ranks: createdCategoriesWithRanks,
-					};
-				}),
-			);
+            return {
+              ...createdCategory,
+              sexes: createdCategoriesWithSexes,
+              ranks: createdCategoriesWithRanks,
+            };
+          }
+        })
+      );
 
-			return createdCategories;
-		});
-	}
+      return createdCategories;
+    });
+  }
 }
