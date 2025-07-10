d65f611d67565655dd82c7152e5763525badcdfe
William <USRSIS0118@ESTSIS0118>
2025-07-10 15:43:14 -0400

    feat(sex): create related usecases

--- apps/api/src/modules/sex/interfaces/controllers/SexController.js
+++ apps/api/src/modules/sex/interfaces/controllers/SexController.js
@@ -1,15 +1,26 @@
 import { databaseService } from "#src/services/database/databaseService.js";
 
 import { CreateSex } from "#sex/application/CreateSex.js";
+import { FindAllSexes } from "#sex/application/FindAllSexes.js";
+
 import { SexDatabaseRepository } from "#sex/infraestructure/SexDatabaseRepository.js";
 
 export class SexController {
-	static async create(req, res) {
-		const repo = new SexDatabaseRepository(databaseService);
-		const useCase = new CreateSex({ repository: repo });
+  static async create(req, res) {
+    const repo = new SexDatabaseRepository(databaseService);
+    const useCase = new CreateSex({ repository: repo });
 
-		const sex = await useCase.execute(req.body);
+    const sex = await useCase.execute(req.body);
 
-		return res.status(201).json(sex);
-	}
+    return res.status(201).json(sex);
+  }
+
+  static async findAll(_, res) {
+    const repo = new SexDatabaseRepository(databaseService);
+    const useCase = new FindAllSexes({ sexRepository: repo });
+
+    const sexes = await useCase.execute();
+
+    return res.status(200).json(sexes);
+  }
 }
--- apps/api/src/modules/sex/interfaces/routes/sex.routes.js
+++ apps/api/src/modules/sex/interfaces/routes/sex.routes.js
@@ -3,6 +3,8 @@ import { SexController } from "#sex/interfaces/controllers/SexController.js";
 
 const sexRouter = Router();
 
+sexRouter.get("/", SexController.findAll);
+
 sexRouter.post("/", SexController.create);
 
 export { sexRouter };
