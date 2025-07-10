653233f49001215e8679c0b677527a304b8999f9
William <williamsuarezdev@gmail.com>
2025-07-10 00:55:48 -0400

    chore(frontend): install primevue forms

--- apps/frontend/vite.config.mjs
+++ apps/frontend/vite.config.mjs
@@ -7,9 +7,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        noDiscovery: true
-    },
     plugins: [
         vue(),
         Components({
