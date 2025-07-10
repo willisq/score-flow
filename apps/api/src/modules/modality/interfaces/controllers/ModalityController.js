711fb26f405e7f9a87ea5b86b744167881f09a16
William <USRSIS0118@ESTSIS0118>
2025-07-10 15:38:27 -0400

    feat(modalities): create use cases related with modalities

--- apps/api/src/modules/modality/interfaces/controllers/ModalityController.js
+++ apps/api/src/modules/modality/interfaces/controllers/ModalityController.js
@@ -1,17 +1,27 @@
 import { CreateModality } from "#modality/application/CreateModality.js";
 import { ModalityDatabaseRepository } from "#modality/infrastructure/ModalityDatabaseRepository.js";
+import { FindAllModalities } from "#modality/application/FindAllModalities.js";
 
 import { databaseService } from "#src/services/database/databaseService.js";
 
 export class ModalityController {
-	static async createModality(req, res) {
-		const repo = new ModalityDatabaseRepository(databaseService);
-		const useCase = new CreateModality({ repository: repo });
+  static async createModality(req, res) {
+    const repo = new ModalityDatabaseRepository(databaseService);
+    const useCase = new CreateModality({ repository: repo });
 
-		await useCase.execute({
-			description: req.body.description,
-		});
+    await useCase.execute({
+      description: req.body.description,
+    });
 
-		res.status(201).json({ message: "Modalidad creada exitosamente" });
-	}
+    res.status(201).json({ message: "Modalidad creada exitosamente" });
+  }
+
+  static async findAll(_, res) {
+    const repo = new ModalityDatabaseRepository(databaseService);
+    const useCase = new FindAllModalities({ modalityRepository: repo });
+
+    const modalities = await useCase.execute();
+
+    return res.status(200).json(modalities);
+  }
 }
